449135fe834b1b5ebe19599090f198a2cf4d9778
mauriliobenevento <maurilio.benevento@gmail.com>
2022-12-21 14:38:47 -0300

    Mudança para pastas

---
+++ JS/js010.js
@@ -0,0 +1,10 @@
+// daqui pra frente utiliza-se node com saida no terminal
+// a saida de console utilizada no browser não funciona para JS. 
+
+var vel = 72.4
+if (vel > 60)
+    console.log("Você ultrassou a velocidade permitida, a MULTA vai pra sua casa")
+else 
+console.log(`A velocidade do seu carro é ${vel} km por hora, continue a sua viagem`)
+
+
